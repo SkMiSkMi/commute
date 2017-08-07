304ea2d7dd67478c7e46301d6b33462ddf26666d
jakedwyer <mjdwyer@gmail.com>
2017-08-07 18:31:06 -0400

    Added arrival time

--- commute.py
+++ commute.py
@@ -1,8 +1,9 @@
 import urllib.request, json
 import csv
-import datetime
-url = "https://maps.googleapis.com/maps/api/directions/json?origin=place_id:%s&destination=place_id:%s&key=%s"
-url2 = "https://maps.googleapis.com/maps/api/directions/json?origin=place_id:%s&destination=place_id:%s&mode=transit&key=%s"
+import datetime, calendar, time
+
+url = "https://maps.googleapis.com/maps/api/directions/json?origin=place_id:%s&destination=place_id:%s&key=%s&arrival_time=%s"
+url2 = "https://maps.googleapis.com/maps/api/directions/json?origin=place_id:%s&destination=place_id:%s&mode=transit&key=%s&arrival_time=%s"
 
 towns = [("Old Greenwich", "ChIJbWgW47aYwokRS_-QioWnp9k"),
 ("Greenwich","ChIJly5MDxGYwokR4jAEYCeuMQg"),
@@ -13,18 +14,19 @@ TwoSigma = "ChIJpf-x64pZwokRYr9BwWaVP9k"
 
 API_Key = "AIzaSyCFgqXKCJF4OS7kqmjiZcECFTX0VM_pK4Y"
 
+
 times = []
 output = []
 train = []
 urls = []
-
+arrival = 1502197200
 
 for (city, locate) in towns:
-    req = url % (locate, TwoSigma, API_Key)
-    req2 = url2 % (locate, TwoSigma, API_Key)
+    req = url % (locate, TwoSigma, API_Key, arrival)
+    req2 = url2 % (locate, TwoSigma, API_Key, arrival)
     train = json.loads(urllib.request.urlopen(req2) .read())
     drive = json.loads(urllib.request.urlopen(req) .read())
-    times.append([city, "Drive: " + drive['routes'][0]['legs'][0]['duration']['text'], "Train: " + train['routes'][0]['legs'][0]['duration']['text'], "Time: " + str(datetime.datetime.now())])
+    times.append([city, "Drive: " + drive['routes'][0]['legs'][0]['duration']['text'], "Train: " + train['routes'][0]['legs'][0]['duration']['text'] + " and Leave by :" + train['routes'][0]['legs'][0]['departure_time']['text'], "Time: " + str(datetime.datetime.now())])
 print(times)
 
 with open("commute.csv", "a") as f:
