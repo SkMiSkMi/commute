18ee3208d74daf32d318a640ef9575b976117a66
jakedwyer <mjdwyer@gmail.com>
2017-08-08 03:32:03 +0000

    starting with mysql

--- commute.py
+++ commute.py
@@ -1,6 +1,14 @@
 import urllib.request, json
 import csv
 import datetime, calendar, time
+#import pymysql
+
+#conn = pymysql.connect(
+#        db='commuting',
+#        user='root',
+#        passwd='gooses',
+#        host='localhost')
+#c = conn.cursor()
 
 url = "https://maps.googleapis.com/maps/api/directions/json?origin=place_id:%s&destination=place_id:%s&key=%s&arrival_time=%s"
 url2 = "https://maps.googleapis.com/maps/api/directions/json?origin=place_id:%s&destination=place_id:%s&mode=transit&key=%s&arrival_time=%s"
@@ -15,7 +23,7 @@ TwoSigma = "ChIJpf-x64pZwokRYr9BwWaVP9k"
 API_Key = "AIzaSyCFgqXKCJF4OS7kqmjiZcECFTX0VM_pK4Y"
 
 
-times = []
+dur  = []
 output = []
 train = []
 urls = []
@@ -26,8 +34,11 @@ for (city, locate) in towns:
     req2 = url2 % (locate, TwoSigma, API_Key, arrival)
     train = json.loads(urllib.request.urlopen(req2) .read())
     drive = json.loads(urllib.request.urlopen(req) .read())
-    times.append([city, "Drive: " + drive['routes'][0]['legs'][0]['duration']['text'], "Train: " + train['routes'][0]['legs'][0]['duration']['text'], "Leave by: " + train['routes'][0]['legs'][0]['departure_time']['text'], "Time: " + str(datetime.datetime.now())])
-print(times)
+    dur.append([city, "Drive: " + drive['routes'][0]['legs'][0]['duration']['text'], "Train: " + train['routes'][0]['legs'][0]['duration']['text'], "Leave by: " + train['routes'][0]['legs'][0]['departure_time']['text'], "Time: " + str(datetime.datetime.now())])
+#    c.execute("INSERT INTO times VALUES (dur)")
+
+print(dur)
+
 
 #with open("commute.csv", "a") as f:
 #    writer = csv.writer(f)
